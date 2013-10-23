4b0328a20707d605c566cf3f6910625dc643b3d0
Matthew Maxwell <m@matthewcmaxwell.com>
2013-10-22 19:51:41 -0500

    Add GUID functionality
    Add GUID functionality.

--- uid.js
+++ uid.js
@@ -1,8 +1,13 @@
-define(function () {
-
-    return function (prefix) {
-        var id = Math.floor((Math.random() * 9999) + 1) + "-" + Math.floor((Math.random() * 9999) + 1) + "-" + Math.floor((Math.random() * 9999) + 1);
-        return prefix ? prefix + "-" + id : id;
-    }
-
+define({
+	uid: function (prefix) {
+			var id = Math.floor((Math.random() * 9999) + 1) + "-" + Math.floor((Math.random() * 9999) + 1) + "-" + Math.floor((Math.random() * 9999) + 1);
+			return prefix ? prefix + "-" + id : id;
+	},
+	guid: function () {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(character) {
+			var rand = Math.random() * 16|0,
+				value = character === 'x' ? rand : (rand&0x3|0x8);
+			return value.toString(16);
+		});
+	}
 });
